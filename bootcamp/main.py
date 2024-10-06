45e6b35e67fc48b37a1eb824b4c047e33f5a686e
Antonin Blot <antonin.blot@crick.ac.uk>
2024-10-06 16:22:02 +0100

    rename too bootcamp

---
+++ bootcamp/main.py
@@ -0,0 +1,15 @@
+from bootcamp.submodule import compute
+
+def complex_analysis(data, offset=1):
+    """Perform a complex analysis on the data and return the result
+
+    Args:
+        data (array): The data to analyze
+        offset (int): An offset to add to the data
+
+    Returns:
+        array: The result of the analysis
+    """
+
+ 
+    return compute.computit(data, offset)
--- bootcamp/submodule/compute.py
+++ bootcamp/submodule/compute.py
@@ -1,3 +1,3 @@
-def calcule_it(a, b):
+def computit(a, b):
     """Return the product of a and b."""
     return a * b
